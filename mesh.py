f1809594ba34d3c9ee6c2b1d7d539281ba5b4b82
Jesus Sierralaya <jesus.sierralaya@hotmail.com>
2024-02-26 21:15:08 +0100

    Creation of scene.py to render objects
    Additionally adds the files of the previous commit

---
+++ mesh.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from vao import VAO
+from texture import Texture
+
+class Mesh:
+    def __init__(self, app):
+        self.app = app
+        self.vao = VAO(app.ctx)
+        self.texture = Texture(app.ctx)
+
+    def destroy(self):
+        self.vao.destroy()
+        self.texture.destroy()
