66445d3ec38f117fde57c357a68313e64d52b846
Jesus Sierralaya <jesus.sierralaya@hotmail.com>
2024-03-15 19:46:39 +0100

    Cleans the main from 'cam_toggle' variables
    This change enhances code organization by making main.py scrip cleaner
    and more focused on core funciontalities. Also improves modularity and
    facilitaes easier maintenance and reuse of the program.

--- custom/cam_toggle.py
+++ custom/cam_toggle.py
@@ -1,21 +1,31 @@
 # cam_toggle.py
 
 """
-Toggles the camera separation based on the current time and the last toggle time.
-    Parameters:
-   - camera: The Camera object whose separation we're toggling.
-   - current_time: The current time in seconds.
-   - last_toggle_time: The last time the camera was toggled.
-   - toggle_interval: The interval between toggles in seconds.
-    Returns:
-   - last_toggle_time: Updated last toggle time if the camera was toggled, otherwise the same as input.
+This module contains functionality to toggle the camera's separation distance based on timing.
 """
 
 from config import cam_separation_1, cam_separation_2
+
+# Initialize the last toggle time as a global variable to maintain state across calls
+last_toggle_time = 0
+
 def toggle_camera(camera, current_time, last_toggle_time, toggle_interval):
+    """
+    Toggles the camera separation based on the current time and the last toggle time.
+
+    Parameters:
+        camera: The Camera object whose separation we're toggling.
+        current_time: The current time in seconds.
+        toggle_interval: The interval between toggles in seconds.
+
+    Returns:
+        Updates the global last_toggle_time if the camera was toggled.
+    """
+    global last_toggle_time
 
     if current_time - last_toggle_time > toggle_interval:
         new_cam_separation = cam_separation_2 if camera.cam_separation == cam_separation_1 else cam_separation_1
         camera.cam_separation = new_cam_separation
         last_toggle_time = current_time
+
     return last_toggle_time
